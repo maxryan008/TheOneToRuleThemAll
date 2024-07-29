480b41800aadb3b8d3ad24895f8059cbc0135dcd
maxryan008 <35043606+maxryan008@users.noreply.github.com>
2024-07-29 20:50:15 +1000

    Base Template
    Base Template of mod done

---
+++ src/main/java/dev/cables/Cables.java
@@ -0,0 +1,16 @@
+package dev.cables;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Cables implements ModInitializer {
+	public static final String MOD_ID = "cables";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
